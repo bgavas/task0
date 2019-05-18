46cfc76f7dbe62afeac6580a5a26d2b83f088805
Burak <burakgavas@gmail.com>
2019-05-18 12:34:33 +0300

    Add server initialization

--- server/server.js
+++ server/server.js
@@ -2,3 +2,20 @@
 require('./config/config.js');
 // Connect to db
 require('./db/connectDb');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT;
+
+// Middlewares
+app.use(bodyParser.json());
+
+// Listen requests
+app.listen(port, () => {
+	console.log(`App started on port ${port}`);
+});
+
+// Export for testing
+module.exports = { app };
